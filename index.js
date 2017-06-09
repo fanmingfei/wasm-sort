ecf0fa544948b312bc2fe781485d3c8264629ebb
明非 <az8641683@163.com>
2017-06-09 15:02:03 +0800

    async

--- index.js
+++ index.js
@@ -2,13 +2,26 @@ import fetchWasm from './utils.js';
 
 var instanceExports;
 var offset;
-fetchWasm('./wasm/algorithm.wasm', {}).then(instance => {
-    instanceExports = instance.exports;
-    offset = instanceExports.getArrayOffset();
-});
+
+var sortsAlgorithm;
+
+
+sortsAlgorithm = {
+    bubbleSort
+};
+
+function algorithm() {
+    if (instanceExports) {
+        return sortsAlgorithm;
+    }
+    return fetchWasm('./wasm/algorithm.wasm', {}).then(instance => {
+        instanceExports = instance.exports;
+        offset = instanceExports.getArrayOffset();
+        return sortsAlgorithm;
+    });
+}
 
 function setArrayBuffer(arr) {
-    console.log(arr)
     let mem = new Uint32Array(instanceExports.memory.buffer, offset, arr.length);
     for (let i = 0; i < arr.length; i++) {
         mem[i] = arr[i];
@@ -31,4 +44,4 @@ function bubbleSort(arr, flag) {
     return _arr;
 }
 
-export { bubbleSort }
+export default algorithm;
